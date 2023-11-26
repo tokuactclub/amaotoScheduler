001a581e2134a85d85918ea5dee11ce60b171468
Ikuma Fukumoto <serpens.github@gmail.com>
2023-11-26 17:12:15 +0900

    change chatbot url

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def main():
     {'destination': 'Ud686a6755685ed53407a76dd183cc82f', 'events': [{'type': 'message', 'message': {'type': 'text', 'id': '483373867147460721', 'quoteToken': 'zpxht856GpsRq9hDkQQHe69DEQ1yxu2KVKjGEW5vs4u2_KOEOPzaaJz0pTMJ7qsNnl-97gVyimImzJrsMYcdvGe5EGMYXsRjVcNS4gSvqRuGxMyY4eedM3GZk5fV8SqfntScw6R-L4uFf-2Na6mQzQ', 'text': 'さ'}, 'webhookEventId': '01HG4354B8PBWJH9AQ74AQKCAC', 'deliveryContext': {'isRedelivery': False}, 'timestamp': 1700944580461, 'source': {'type': 'user', 'userId': 'Ue99cbbb0fb4a6cb510c2ea5f343f6715'}, 'replyToken': 'fb40266f492d4e2e8963926a593c1914', 'mode': 'active'}]}
     msg : str = body["events"][0]["message"]["text"]
     if msg.startswith("あまおとくん"):
-        serviceURL = ['https://gpt-bot.userlocal.jp/api/webhook/e8449bb8','https://script.google.com/macros/s/AKfycby-2Fmm9VymDqU5cEnzadScSkmCoosUKlxhcTgPD9KjNliMpiNA8cfLQO-ZLOrzP0MOxQ/exec']
+        serviceURL = ['https://gpt-bot.userlocal.jp/api/webhook/2c7e8d28','https://script.google.com/macros/s/AKfycby-2Fmm9VymDqU5cEnzadScSkmCoosUKlxhcTgPD9KjNliMpiNA8cfLQO-ZLOrzP0MOxQ/exec']
         for url in serviceURL:
             print(f"START ACCESS TO {url}")
             response = webhook(request,url)
