a5762f08c1d516c1544e9b351a9b153f173a5e1d
Ikuma Fukumoto <serpens.github@gmail.com>
2024-01-14 17:09:40 +0900

    list and all option

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # amaotoScheduler
+
+ama reminder -num
+ama schedule
+
+追加予定
+-を~に変更？(負の数に対応)
+ama reminder -all
+ama practice
+ama schedule(変更)
+ama reminder list#indexなりなんなり
+ama reminder delete index
+ama reminder set (date or num)
+
+ama schedule (auto or manual )
+
+ama help
--- src/transformer/main.py
+++ src/transformer/main.py
@@ -3,7 +3,10 @@ from lark import Transformer
 class MyTransformer(Transformer):
 
     def __init__(self, visit_tokens: bool = True) -> None:
-        self.options = {}
+        self.options = {
+            "list":False,
+            "all":False
+            }
         super().__init__(visit_tokens)
 
     def script(self,items):
@@ -25,11 +28,17 @@ class MyTransformer(Transformer):
     def reminder_date(self,items):
         self.options["remindDate"] = [items[0]]
 
+
     ##schedule
 
     #オプションの種類
     def option_number(self,items):
         return items[0]
+    def option_list(self,items):
+        self.options["list"]=True
+    def option_all(self,items):
+        self.options["all"]=True
+        return
     
     #よみこみ
     
